fad1a5950fad5c6c85fdbe32f6eb76a37bb077ad
Mekki <yacinemk81@gmail.com>
2026-04-01 18:53:24 +0100

    fix: remove broken hybrid.py import, add cnn_bilstm_transformer to notebook models, fix phase count display

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,14 +1,19 @@
 from .lstm import LSTMClassifier
-from .transformer import TransformerClassifier
+from .transformer import TransformerClassifier, NLPTransformerClassifier
 from .cnn_lstm import CNNLSTMClassifier, CNNClassifier
 from .xgboost_lstm import XGBoostLSTMClassifier
 from .cnn_bilstm_transformer import CNNBiLSTMTransformerClassifier
+from .bilstm import BiLSTMClassifier
+from .cnn_bilstm import CNNBiLSTMClassifier
 
 __all__ = [
     "LSTMClassifier",
     "TransformerClassifier",
+    "NLPTransformerClassifier",
     "CNNLSTMClassifier",
     "CNNClassifier",
     "XGBoostLSTMClassifier",
     "CNNBiLSTMTransformerClassifier",
+    "BiLSTMClassifier",
+    "CNNBiLSTMClassifier",
 ]
